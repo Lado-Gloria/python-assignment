2b8eeb80d512d009eba4ff5b7a8ab25d202255d4
Gloria-Lado <glorialadoo@gmail.com>
2023-03-31 16:07:18 +0300

    files

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def sub(c,d):
+    result = c-d
+    return result
+
+def divi(e,f):
+    results = e/f
+    return results
+
+def mult(g,h):
+    result1 = g*h
+    return result1
+
+def remain(i,j):
+    rem =i%j
+    return rem
